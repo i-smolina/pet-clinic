b09c87cdebb5332b187c0fac4c6f1cf89006d80a
i-smolina <smolirinka@gmail.com>
2021-01-08 13:29:17 +0500

    Initial Project Add

---
+++ src/main/java/guru/springframework/petclinic/model/Pet.java
@@ -0,0 +1,33 @@
+package guru.springframework.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private String name;
+    private PetType petType;
+    private LocalDate birthDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
