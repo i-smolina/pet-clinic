4ea0cd547db8a98c9a200806a01f34fea4557d25
i-smolina <smolirinka@gmail.com>
2021-01-17 14:14:16 +0500

    adding list of Owners on Owners Page. Closes #16

--- pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/OwnersController.java
@@ -1,14 +1,23 @@
 package guru.springframework.petclinic.controllers;
 
+import guru.springframework.petclinic.services.map.OwnerServiceMap;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnersController {
 
+    private final OwnerServiceMap ownerServiceMap;
+
+    public OwnersController(OwnerServiceMap ownerServiceMap) {
+        this.ownerServiceMap = ownerServiceMap;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerServiceMap.findAll());
         return "owners/index";
     }
 }
