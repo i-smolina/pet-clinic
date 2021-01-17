8d83f96b536ebe7fa9f2391e8dadd96b9a56fe7f
i-smolina <smolirinka@gmail.com>
2021-01-17 14:22:41 +0500

    adding list of Vets on Vets Page. Closes #15

--- pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package guru.springframework.petclinic.controllers;
 
+import guru.springframework.petclinic.services.VetService;
+import guru.springframework.petclinic.services.map.VetServiceMap;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetServiceMap vetServiceMap;
+
+    public VetController(VetServiceMap vetServiceMap) {
+        this.vetServiceMap = vetServiceMap;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetServiceMap.findAll());
         return "vets/index";
     }
 }
