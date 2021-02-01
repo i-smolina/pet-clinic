7cddbfaac3444e0a2481867b7695293a0286840d
i-smolina <smolirinka@gmail.com>
2021-02-01 14:34:23 +0500

    added Pets and Contact Info to Owners on startup. Closes #32

--- pet-clinic-web/src/main/java/guru/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package guru.springframework.petclinic.bootstrap;
 
 import guru.springframework.petclinic.model.Owner;
+import guru.springframework.petclinic.model.Pet;
 import guru.springframework.petclinic.model.PetType;
 import guru.springframework.petclinic.model.Vet;
 import guru.springframework.petclinic.services.OwnerService;
@@ -10,6 +11,8 @@ import guru.springframework.petclinic.services.map.OwnerServiceMap;
 import guru.springframework.petclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -28,20 +31,42 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
         PetType dog = new PetType();
         dog.setName("Собака");
-        PetType sevedDogPetType = petTypeService.save(dog);
+        PetType savedDogPetType = petTypeService.save(dog);
 
         PetType cat = new PetType();
         cat.setName("Кошка");
-        PetType sevedCatPetType = petTypeService.save(cat);
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Иван");
         owner1.setLastName("Иванов");
+        owner1.setSity("Москва");
+        owner1.setAddress("Полевая 105 к5 кв 93");
+        owner1.setTelephone("84951234569");
+
+        Pet ivanovPet = new Pet();
+        ivanovPet.setName("Шарик");
+        ivanovPet.setPetType(savedDogPetType);
+        ivanovPet.setBirthDate(LocalDate.now());
+        ivanovPet.setOwner(owner1);
+        owner1.getPets().add(ivanovPet);
+
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Александр");
         owner2.setLastName("Пушкин");
+        owner2.setSity("Санкт-Петербург");
+        owner2.setAddress("Парашутная д.100 кв 90");
+        owner2.setTelephone("88121234578");
+
+        Pet pushkinPet = new Pet();
+        pushkinPet.setName("Мурка");
+        pushkinPet.setPetType(savedCatPetType);
+        pushkinPet.setBirthDate(LocalDate.now());
+        pushkinPet.setOwner(owner2);
+        owner2.getPets().add(pushkinPet);
+
         ownerService.save(owner2);
 
         System.out.println("Loaded owners....");
