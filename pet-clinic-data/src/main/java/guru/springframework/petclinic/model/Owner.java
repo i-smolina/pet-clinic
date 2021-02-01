f1a6cf17992d988f45c34782f00e889f00578445
i-smolina <smolirinka@gmail.com>
2021-02-01 13:35:43 +0500

    added address, city, telephone to Owner Entity. Closes #27

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
@@ -4,7 +4,34 @@ import java.util.Set;
 
 public class Owner extends Person{
 
+    private String address;
+    private String sity;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSity() {
+        return sity;
+    }
+
+    public void setSity(String sity) {
+        this.sity = sity;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
